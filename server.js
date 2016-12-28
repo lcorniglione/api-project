6c1b7438791f3c61cf6bdc212d9f4c7f06d951de
Luciano Corniglione <luciano.corniglione.minig@intel.com>
2016-12-28 15:16:55 +0000

    Solution

--- server.js
+++ server.js
@@ -1,11 +1,52 @@
 var express = require("express");
+var url = require("url");
+var app = express();
 
-var app = express()
 
-app.get('/', function (req, res) {
-  res.send('dhbahdbha')
-})
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get("/:month%20:day,%20:year", function(request, response) {
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    var day = request.params.day;
+    var month = request.params.month;
+    var year = request.params.year;
+    var date = (month + " " + day + " " + year).toString();
+    var dateObj = new Date(date);
+    var unixtime = Date.parse(date) / 1000;
+    var obj = new Object();
+    obj.unix = unixtime;
+    obj.natural = (monthNames[dateObj.getMonth()] + " " + day + "," + " " + year).toString();
+    response.send(JSON.stringify(obj));
+});
+
+app.get("/:unix", function(request, response) {
+    if (typeof parseInt(request.params.unix)==='number' && (request.params.unix%1)===0) {
+        var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        var dateObj = new Date(request.params.unix * 1000);
+        var obj = new Object();
+        obj.unix = request.params.unix;
+        obj.natural = (monthNames[dateObj.getMonth()] + " " + dateObj.getDate() + "," + " " + dateObj.getFullYear()).toString();
+        response.send(JSON.stringify(obj));
+    }
+    else {
+        var obj = new Object();
+        obj.unix = null;
+        obj.natural = null;
+        response.send(JSON.stringify(obj));
+    }
+
+});
+
+app.get('*', function(request, response) {
+    var obj = new Object();
+    obj.unix = null;
+    obj.natural = null;
+    response.send(JSON.stringify(obj));
+});
+
+
+
+app.listen(8080);
